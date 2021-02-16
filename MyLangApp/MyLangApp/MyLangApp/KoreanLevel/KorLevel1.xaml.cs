819a21b6204ebf2e1cb9be8a78b776b51d4db033
Anna <anna.h.hallberg.92@gmail.com>
2021-02-16 15:35:53 +0100

    Create Level pages

---
+++ MyLangApp/MyLangApp/MyLangApp/KoreanLevel/KorLevel1.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyLangApp.KoreanLevel
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class KorLevel1 : ContentPage
+    {
+        public KorLevel1()
+        {
+            InitializeComponent();
+        }
+    }
+}
